56b07c39d0e131659fab7bd39dacbd881f4b8604
hopecarroll <124173733+hopecarroll@users.noreply.github.com>
2023-02-15 10:22:59 -0500

    created abstract shape using functions, if else statements and an added ifColor variable

---
+++ lab3exercise3.js
@@ -0,0 +1,26 @@
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  background(220);
+  drawAbstractShape(200,200,1,"green");
+}
+function drawAbstractShape(x,y,size,ifColor) {
+  strokeWeight(5);
+  stroke(ifColor);
+  if (mouseIsPressed) {
+    stroke("yellow");
+  } else if (mouseX >= 100 && mouseX <= 300) {
+    stroke("blue");
+  } else if (mouseX > 300) {
+    stroke("purple");
+  }
+  //abstract shape
+  line(200, 200, 350, 100);
+  line(200, 200, 200, 50);
+  line(200, 200, 50, 100);
+  line(200, 200, 50, 280);
+  line(200, 200, 200, 330);
+  line(200, 200, 350, 300);
+}
